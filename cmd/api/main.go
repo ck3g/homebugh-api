5591f069b631acc6d50c5a9a19ce5e26533d43eb
Vitali Tatarintev <vtatarintev@gitlab.com>
2020-10-25 19:40:11 +0100

    Extract TLS settings into config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,12 +2,41 @@ package main
 
 import (
 	"crypto/tls"
+	"flag"
+	"fmt"
 	"net/http"
+	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 type application struct{}
 
+type config struct {
+	TLS struct {
+		CertPemFile string `yaml:"cert_pem_file"`
+		KeyPemFile  string `yaml:"key_pem_file"`
+	} `yaml:"tls"`
+}
+
 func main() {
+	configFile := flag.String("config", "./config.yml", "The app configuration file path")
+	flag.Parse()
+
+	f, err := os.Open(*configFile)
+	if err != nil {
+		flag.PrintDefaults()
+		panic("cannot load the config file")
+	}
+	defer f.Close()
+
+	var cfg config
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		panic("cannot parse the config file")
+	}
+
 	app := &application{}
 
 	tlsConfig := &tls.Config{
@@ -21,5 +50,9 @@ func main() {
 		TLSConfig: tlsConfig,
 	}
 
-	srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
+	fmt.Printf("Listening on %s, CTRL+C to stop\n", srv.Addr)
+	err = srv.ListenAndServeTLS(cfg.TLS.CertPemFile, cfg.TLS.KeyPemFile)
+	if err == nil {
+		panic("Cannot start the API server")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ck3g/homebugh-api
 
 go 1.15
+
+require gopkg.in/yaml.v2 v2.3.0
