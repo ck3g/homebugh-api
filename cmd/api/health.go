2a444114c06d3cbcbfd5f5f355fc23ccdbbf0d10
Vitali Tatarintev <Kalastiuz@gmail.com>
2021-09-06 14:59:51 +0200

    Display version metadata in health endpoint

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -4,7 +4,9 @@ import "net/http"
 
 func (app *application) healthHandler(w http.ResponseWriter, r *http.Request) {
 	env := envelope{
-		"status": "OK",
+		"status":    "OK",
+		"version":   app.metadata.version,
+		"buildTime": app.metadata.buildTime,
 	}
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
